c8c59d9db3b679995bb11ee9dbbce0e7a1dd5f1a
Uchijo <kgtnttki.yuta@gmail.com>
2023-09-24 22:59:09 +0900

    main.goも書き換えた

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/uchijo/walica-clone-backend/data"
+	"github.com/uchijo/walica-clone-backend/presenter"
+	apipb "github.com/uchijo/walica-clone-backend/proto/proto/api"
+	"github.com/uchijo/walica-clone-backend/util"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func init() {
+	util.LoadEnv()
+	util.ConnectToDB()
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("faled to listen:", err)
+	}
+
+	s := grpc.NewServer()
+	repository := data.RepositoryImpl{}
+	apipb.RegisterWalicaCloneApiServer(s, presenter.NewServer(repository))
+	log.Println("serving gRPC on 0.0.0.0:8080")
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:8080",
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	err = apipb.RegisterWalicaCloneApiHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
+}
